3c6cf5e14f2e98bbd8da32e1078f236d699142cb
behradk90@gmail.com <behradk90@gmail.com>
2026-03-23 20:57:34 +0000

    Links added

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -52,14 +52,33 @@ export default function page() {
             </button>
           </form>
         </div>
-        <div className="w-full text-left">
-          Click here to contact Tom directly via this website - though replies
-          take a while at busy times. You can also follow and message him via
-          Twitter and Instagram.
+        <div className="w-full">
+          <a href="" className="font-semibold text-blue-950 hover:text-red-500">
+            {" "}
+            Click here{" "}
+          </a>{" "}
+          to contact Tom directly via this website - though replies take a while
+          at busy times. You can also follow and message him via{" "}
+          <a href="" className="font-semibold text-blue-950 hover:text-red-500">
+            {" "}
+            Twitter{" "}
+          </a>
+          and{" "}
+          <a href="" className="font-semibold text-blue-950 hover:text-red-500">
+            {" "}
+            Instagram
+          </a>
+          .
         </div>
-        <div className="w-full text-center">
-          Management address: Leporine Ltd, PO Box 3185, London SW18 3JG. For
-          business and media enquiries click here to email the office.
+        <div className="w-full">
+          Management address:{" "}
+          <b>Leporine Ltd, PO Box 3185, London SW18 3JG. </b> For business and
+          media enquiries
+          <a href="" className="font-semibold text-blue-950 hover:text-red-500">
+            {" "}
+            click here{" "}
+          </a>
+          to email the office.
         </div>
       </div>
     </div>
