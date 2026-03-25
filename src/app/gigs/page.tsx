3f5443bfe7647c7ea87c5386bccff1f2aea7ac94
behradk90@gmail.com <behradk90@gmail.com>
2026-03-25 21:35:08 +0000

    Made responsive

--- src/app/gigs/page.tsx
+++ src/app/gigs/page.tsx
@@ -5,31 +5,35 @@ export default function page() {
     <div className="space-y-2">
       {upcomingGigs.map((gig) => (
         <div
-          className="w-full bg-blue-400 p-5 flex space-x-1"
+          className="w-full bg-blue-400 p-2 md:p-5 flex space-x-1"
           key={gig.date + gig.venue}
         >
-          <div className="pr-2 text-lg font-semibold text-slate-700">
-            {gig.date}
-          </div>
-          <div className="space-y-2">
-            <div className="flex items-center space-x-1">
-              <div className="text-lg font-bold">{gig.city.toUpperCase()}:</div>
-              <a
-                href={gig.link}
-                target="_blank"
-                className="underline hover:no-underline"
-              >
-                {gig.venue}
-                {gig.sale ? `[${gig.sale.toUpperCase()}]` : null}
-              </a>
+          <div className="md:flex">
+            <div className="pr-2 md:text-xl text-slate-700 md:w-24">
+              {gig.date}
             </div>
-            <div className="text-sm text-slate-700">{gig.extraDetails}</div>
+            <div className="md:space-y-2 w-full">
+              <div className="flex flex-wrap items-center gap-x-1 w-full">
+                <div className="text-lg font-bold">
+                  {gig.city.toUpperCase()}:
+                </div>
+
+                <a
+                  href={gig.link ?? undefined}
+                  target="_blank"
+                  className="underline hover:no-underline"
+                >
+                  {gig.venue} {gig.sale ? `[${gig.sale.toUpperCase()}]` : null}
+                </a>
+              </div>
+              <div className="text-sm text-slate-700">{gig.extraDetails}</div>
+            </div>
           </div>
         </div>
       ))}
-      <div className="w-full flex space-x-8 bg-blue-400 p-2 py-8">
+      <div className="w-full space-y-4 md:space-y-0 md:flex space-x-8 bg-blue-400 p-4 py-8">
         <h2 className="text-xl font-semibold">Line Ups</h2>
-        <div className="space-y-4 divide-y col-span-2 max-w-80">
+        <div className="space-y-4 divide-y col-span-2">
           <h2 className="text-xl font-semibold">
             Tom Robinson Band (Full Band Show)
           </h2>
@@ -56,7 +60,7 @@ export default function page() {
             </div>
           </div>
         </div>
-        <div className="space-y-4 divide-y col-span-2 max-w-80">
+        <div className="space-y-4 divide-y col-span-2">
           <h2 className="text-xl font-semibold">Acoustic Duo Show</h2>
           <div className="space-y-2 px-1">
             <div className="text-slate-700">
